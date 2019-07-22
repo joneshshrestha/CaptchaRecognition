132d52a33d1a24921b0b03fa2ac580c947fe994e
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-22 21:33:52 +0545

    update

--- extract_single_letters_from_captchas.py
+++ extract_single_letters_from_captchas.py
@@ -37,7 +37,7 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
     contours = contours[0] if imutils.is_cv2() else contours[1]
 
     letter_image_regions = []
-    
+
     # Now we can loop through each of the four contours and extract the letter
     # inside of each one
     for contour in contours:
@@ -55,3 +55,36 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
         else:
             # This is a normal letter by itself
             letter_image_regions.append((x, y, w, h))
+            
+    # If we found more or less than 4 letters in the captcha, our letter extraction
+    # didn't work correcly. Skip the image instead of saving bad training data!
+    if len(letter_image_regions) != 4:
+        continue
+
+    # Sort the detected letter images based on the x coordinate to make sure
+    # we are processing them from left-to-right so we match the right image
+    # with the right letter
+    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+
+    # Save out each letter as a single image
+    for letter_bounding_box, letter_text in zip(letter_image_regions, captcha_correct_text):
+        # Grab the coordinates of the letter in the image
+        x, y, w, h = letter_bounding_box
+
+        # Extract the letter from the original image with a 2-pixel margin around the edge
+        letter_image = gray[y - 2:y + h + 2, x - 2:x + w + 2]
+
+        # Get the folder to save the image in
+        save_path = os.path.join(OUTPUT_FOLDER, letter_text)
+
+        # if the output directory does not exist, create it
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # write the letter image to a file
+        count = counts.get(letter_text, 1)
+        p = os.path.join(save_path, "{}.png".format(str(count).zfill(6)))
+        cv2.imwrite(p, letter_image)
+
+        # increment the count for the current key
+        counts[letter_text] = count + 1
