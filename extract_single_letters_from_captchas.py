b26e082d692965187b6f27e7076e9aa027f4f5f2
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-22 19:01:32 +0545

    update

--- extract_single_letters_from_captchas.py
+++ extract_single_letters_from_captchas.py
@@ -37,3 +37,21 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
     contours = contours[0] if imutils.is_cv2() else contours[1]
 
     letter_image_regions = []
+    
+    # Now we can loop through each of the four contours and extract the letter
+    # inside of each one
+    for contour in contours:
+        # Get the rectangle that contains the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        # Compare the width and height of the contour to detect letters that
+        # are conjoined into one chunk
+        if w / h > 1.25:
+            # This contour is too wide to be a single letter!
+            # Split it in half into two letter regions!
+            half_width = int(w / 2)
+            letter_image_regions.append((x, y, half_width, h))
+            letter_image_regions.append((x + half_width, y, half_width, h))
+        else:
+            # This is a normal letter by itself
+            letter_image_regions.append((x, y, w, h))
